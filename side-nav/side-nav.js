f01d2e55cc06a0a637febfbf9839ff4f15229fd0
Ryan Westlake <ryancwestlake@gmail.com>
2017-02-06 17:40:32 -0700

    [ADD]: transition side nav on click

--- side-nav/side-nav.js
+++ side-nav/side-nav.js
@@ -2,7 +2,21 @@
 
 class SideNav {
   constructor() {
-    
+    console.log('sidenav')
+    this.toggleButtonEl = document.querySelector('.js-menu')
+    this.sideNaveEl = document.querySelector('.js-side-nav')
+
+    this.showSideNav = this.showSideNav.bind(this)
+
+    this.addEventListeners()
+  }
+
+  addEventListeners() {
+    this.toggleButtonEl.addEventListener('click', this.showSideNav)
+  }
+
+  showSideNav() {
+    this.sideNaveEl.classList.add('side-nav--visible')
   }
 }
 
